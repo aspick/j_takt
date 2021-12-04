c7753fb651da64ac5ea7bc38d904b726909fce39
Yugo TERADA <yugo@18th-lab.com>
2021-12-04 22:55:14 +0900

    bundle gem

---
+++ lib/j_takt.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "j_takt/version"
+
+module JTakt
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/j_takt/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module JTakt
+  VERSION = "0.1.0"
+end
